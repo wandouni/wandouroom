b66340dcd220ab6556506c79130a7a714e549347
shenjj@tsintergy.com <shenjj@tsintergy.com>
2021-02-04 20:48:29 +0800

    init

---
+++ src/main/java/com/example/wandouroom/WandouroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.wandouroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WandouroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WandouroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wandouroom/WandouroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wandouroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WandouroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
